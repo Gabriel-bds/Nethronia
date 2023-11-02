40e1bba37d398a468d63d13b962f9c31418b7909
Gabriel <syen.software@gmail.com>
2023-11-01 22:52:52 -0300

    Congelar jogo ao pausar

--- Assets/Scripts/Menus/Menus.cs
+++ Assets/Scripts/Menus/Menus.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class Menus : MonoBehaviour
 {
     [SerializeField] GameObject _menuPersonagem;
+    float _tempoAtual;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
             if(_menuPersonagem.activeInHierarchy) 
-            { 
+            {
+                PausarJogo(false);
                 _menuPersonagem.SetActive(false);
             }
             else
             {
+                PausarJogo(true);
                 _menuPersonagem.SetActive(true);
             }
+        }
+    }
+
+    void PausarJogo(bool _pausado)
+    {
+        if(_pausado) 
+        {
+            _tempoAtual = Time.timeScale;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = _tempoAtual;
         }
     }
 }

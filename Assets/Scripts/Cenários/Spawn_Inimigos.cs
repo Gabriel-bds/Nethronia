5d8678797c6ca79cd3005fb10ed2f539460f32cc
Gabriel <syen.software@gmail.com>
2023-11-30 20:08:05 -0300

    Sistema de spawn de inimigos

--- Assets/Scripts/Cenários/Spawn_Inimigos.cs
+++ Assets/Scripts/Cenários/Spawn_Inimigos.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class Spawn_Inimigos : MonoBehaviour
 {
-    CircleCollider2D _area;
     [SerializeField] GameObject[] _inimigos;
-    [SerializeField] int _quantidade;
-    // Start is called before the first frame update
+    [SerializeField] float _tamanhoArea;
+    [SerializeField] int _qtdSpawns;
+
     private void Awake()
     {
-        _area = GetComponent<CircleCollider2D>();
         SpawnarInimigos();
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, _tamanhoArea);
     }
     void SpawnarInimigos()
     {
-        for(int n = 0; n < _quantidade; n++)
+        for (int n = 0; n < _qtdSpawns; n++)
         {
-            Instantiate(_inimigos[Random.Range(0, _inimigos.Length)], SortearArea(_area), transform.localRotation);
+            Instantiate(_inimigos[Random.Range(0, _inimigos.Length)], SortearArea(), transform.localRotation);
         }
     }
-    Vector2 SortearArea(CircleCollider2D _area)
+
+    Vector2 SortearArea()
     {
-        return new Vector2(Random.Range(_area.bounds.min.x * _area.radius, _area.bounds.max.x * _area.radius),
-                           Random.Range(_area.bounds.min.y * _area.radius, _area.bounds.max.y * _area.radius));
+        return transform.position + Random.onUnitSphere * Random.Range(1, _tamanhoArea);
     }
 }

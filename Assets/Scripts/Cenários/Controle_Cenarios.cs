c8c37449b9e38f91bbbeb0ca623ba9f4541d5356
Gabriel <Gabriel@DESKTOP-BN1OLUI>
2023-12-18 23:12:33 -0300

    Sistema de troca de cenario

---
+++ Assets/Scripts/Cenários/Controle_Cenarios.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controle_Cenarios : MonoBehaviour
+{
+    [SerializeField] int _fase;
+    [SerializeField] int _qtdCenarios;
+    [SerializeField] int _cenarioAtual;
+    [SerializeField] List<int> _cenariosDisponiveis = new List<int>();
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        for (int n = 1; n <= _qtdCenarios; n++)
+        {
+            _cenariosDisponiveis.Add(n);
+        }
+        //SortearCenario();
+    }
+
+    public void SortearCenario()
+    {
+        int _novaCenario = _cenariosDisponiveis[Random.Range(1, _cenariosDisponiveis.Count)];
+        SceneManager.LoadSceneAsync($"{_fase}.{_novaCenario}", LoadSceneMode.Single);
+        _cenariosDisponiveis.Remove(_novaCenario);
+        _cenarioAtual = _novaCenario;
+    }
+}
---
+++ Assets/Scripts/Cenários/Troca_Cenario.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Troca_Cenario : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            FindAnyObjectByType<Controle_Cenarios>().SortearCenario();
+        }
+    }
+}
